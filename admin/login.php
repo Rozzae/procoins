5b0dfba3e149ae41605195b2f8d9b86f13681bcb
uchechukwueze70@gmail.com <uchechukwueze70@gmail.com>
2019-05-14 23:32:13 +0100

    Initial Commit

---
+++ admin/login.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Procoins | The Best Peer-To-Peer fundraising Service</title>
+        <!--Favicon add-->
+        <link rel="shortcut icon" type="image/png" href="../assets/images/icon.png">
+        <!--bootstrap Css-->
+        <link href="../assets/css/plugins/bootstrap.min.css" rel="stylesheet">
+        <!--font-awesome Css-->
+        <link href="../assets/css/font-awesome.min.css" rel="stylesheet">
+        <!--Swiper  Css-->
+        <link href="../assets/css/plugins/swiper.min.css" rel="stylesheet">
+                <!--Lightcase  Css-->
+        <link href="../assets/css/plugins/lightcase.css" rel="stylesheet">
+        <!--Style Css-->
+        <link href="../assets/css/style.css" rel="stylesheet">
+    </head>
+    
+    <body style="background-color:grey; color:black;">
+    <div class="container">
+    <section class="contact-form-section contact-form-section2 section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-push-3">
+                <div class="contact-form contact-form1 contact-form2 ">
+                            <h4> PROCOINS| ADMIN Login</h4>
+                            <form action="#">
+                              <div class="row">
+                                  <div class="col-md-12">
+                                    <div class="form-group">
+                                        <input type="text" name="username" placeholder="Username" required>
+                                    </div>
+                                  </div>
+                                  <div class="col-md-12">
+                                    <div class="form-group">
+                                        <input type="password" name="password" placeholder="Password" required>
+                                    </div>
+                                  </div>
+                                  <div class="col-md-12">
+                                    <div class="form-group">
+                                        <input type="submit" value="Submit">
+                                    </div>
+                                  </div>
+                              </div>
+                            </form>  
+                        </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    </div>
+        <!-- Footer section start -->
+        <footer class="footer-section ">
+
+        <div class="clearfix"></div>
+
+            <!-- Footer copyright -->
+            <div class="footer-copyright">
+                <div class="footer-copyright1">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <p>© 2019 <a href="#">Procoins</a>. All rights reserved.</p>
+                            </div>
+                            
+                        </div><!-- row -->
+                    </div><!-- container -->
+                </div>  <!-- Footer copyright1 -->
+            </div><!-- Footer copyright -->
+            <div id="back-to-top" class="scroll-top back-to-top" data-original-title="" title="" >
+                <i class="fa fa-angle-up"></i>
+            </div>
+        </footer><!-- Footer -->
+
+    <!--==================================
+    =============== Js File ===========
+    ===================================-->
+        <!--jquery script load-->
+        <script src="../assets/js/plugins/jquery.js"></script>
+        <!--Bootstrap v3 script load here-->
+        <script src="../assets/js/plugins/bootstrap.min.js"></script>
+        <!--Wow script load-->
+        <script src="../assets/js/plugins/swiper.min.js"></script>                    
+        <!--Slick Nav Js File Load-->
+        <script src="../assets/js/plugins/lightcase.js"></script>
+        <!--CounterUp script load-->
+        <script src="../assets/js/plugins/jquery.countdown.min.js"></script>
+        <!--Countdown script load-->
+        <script src="../assets/js/plugins/jquery.counterup.min.js"></script>
+        <!--Waypoint script load-->
+        <script src="../assets/js/plugins/waypoints.min.js"></script>
+        <!-- Isotope Script Load -->
+        <script src="../assets/js/plugins/isotope.pkgd.min.js"></script>
+        <!-- Custom Isotope Script Load -->
+        <script src="../assets/js/plugins/custom-isotope.js"></script>
+        <!--Main js file load-->
+        <script src="../assets/js/custom.js"></script>
+    </body>
+
+<!-- Mirrored from thesoftking.com/html/nobility/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 10 May 2019 10:39:12 GMT -->
+</html>
